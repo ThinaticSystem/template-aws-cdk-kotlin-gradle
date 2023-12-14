333d9f98dc709bc88b9aff51882b1c283d6fd8b8
ThinaticSystem <git@thinaticsystem.com>
2023-12-15 01:19:40 +0900

    add example test code (Java to Kotlin)

---
+++ src/test/kotlin/org/example/CdkTest.kt
@@ -0,0 +1,23 @@
+package org.example
+
+//import org.junit.jupiter.api.Test
+//import software.amazon.awscdk.App
+//import software.amazon.awscdk.assertions.Template
+
+// example test. To run these tests, uncomment this file, along with the
+// example resource in java/src/main/java/com/myorg/CdkStack.java
+//class CdkTest {
+//    @Test
+//    fun testStack() {
+//        val app = App()
+//        val stack = CdkStack(app, "test")
+
+//        val template = Template.fromStack(stack)
+
+//        template.hasResourceProperties(
+//            "AWS::SQS::Queue", mapOf<String, Number>(
+//                "VisibilityTimeout" to 300,
+//            )
+//        )
+//    }
+//}
