afbb47557cd7b604e20593a36b4d757561925fff
ThinaticSystem <git@thinaticsystem.com>
2024-01-26 22:56:45 +0900

    upgrade deps
    refactor version management

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
 }
 
 val packageName = "org.example"
@@ -13,9 +13,9 @@ repositories {
 
 dependencies {
     // AWS CDK
-    val awsCdkVersion = "2.115.0"
+    val awsCdkVersion: String by project
     implementation("software.amazon.awscdk:aws-cdk-lib:$awsCdkVersion")
-    val constructsVersion = "10.3.0"
+    val constructsVersion: String by project
     implementation("software.constructs:constructs:$constructsVersion")
 
     // Testing
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,13 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion: String by settings
+        kotlin("jvm") version kotlinVersion
+
+        val shadowVersion: String by settings
+        id("com.github.johnrengelman.shadow") version shadowVersion
+    }
+}
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "cdk_source"
